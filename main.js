d6d0fed9e0c77e01e1d5ea88dceb4d75360ebb2d
Andrew Ward <redrawdawn@gmail.com>
2022-02-18 11:29:50 -0600

    AW added birthdays function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //Blah blah blah
+const everyonesBirthdays = () => { 
+     const awBirthday = ("Andrew's birthday is January 7th, 2004")
+     const csBirthday = ("Cory's birthday is September 18th, 1989")
+     const jaBirthday = ("Jordan's birthday is December 23, 1993")
+     console.log(awBirthday)
+     console.log(csBirthday)
+     console.log(jaBirthday)
+}
+everyonesBirthdays()
